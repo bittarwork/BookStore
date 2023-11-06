ea8ce4b51386664c8788fb9ec93a792d6df88503
bittarwork <81069087+bittarwork@users.noreply.github.com>
2023-11-07 00:08:40 +0200

    finishing first verssion of backend server

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
-export const PORT = 3001;
+export const PORT = 3000;
 export const conecctionString = "mongodb://localhost:27017/"
--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,18 @@ import express, { request, response } from "express";
 import mongoose from "mongoose";
 import { PORT, conecctionString } from "./config.js";
 import { bookRouter } from "./routes/book.js";
-
+import cors from "cors";
 
 const app = express();
 /!* ------------------------------- meddilware ------------------------------- */;
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["content-type"],
+  })
+);
 app.use((request, response, next) => {
   console.log(request.url);
   next();
